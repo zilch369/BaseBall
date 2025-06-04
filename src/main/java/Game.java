0ffdf359ae03a7784e2899a8fc40a47bdcc1a8a4
jk369 <jk369.park@gmail.com>
2025-06-04 13:53:38 +0900

    [refactring] 41p

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,9 @@
 public class Game {
     public void guess(String guessNumber) {
+        assertIllegalArgument(guessNumber);
+    }
+
+    private static void assertIllegalArgument(String guessNumber) {
         if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
@@ -14,10 +18,14 @@ public class Game {
             }
         }
 
-        if (guessNumber.charAt(0) == guessNumber.charAt(1) ||
-            guessNumber.charAt(0) == guessNumber.charAt(2) ||
-            guessNumber.charAt(1) == guessNumber.charAt(2)) {
+        if (isDuplicateNumber(guessNumber)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static boolean isDuplicateNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1) ||
+                guessNumber.charAt(0) == guessNumber.charAt(2) ||
+                guessNumber.charAt(1) == guessNumber.charAt(2);
     }
 }
